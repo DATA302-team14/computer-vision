40f3b71da100ada3c41621244371bef74dac1c6e
uijinee <abrahamkim98@gmail.com>
2024-05-10 14:26:47 +0900

    ~

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -1,12 +1,14 @@
 import cv2
+import os
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from albumentations.pytorch import ToTensorV2
 
 class CustomDataset(Dataset):
-    def __init__(self, csv_file, transform=None, infer=False):
-        self.data = pd.read_csv(csv_file)
+    def __init__(self, data_root, csv_file, transform=None, infer=False):
+        self.data_root = data_root
+        self.data = pd.read_csv(os.path.join(data_root, csv_file))
         self.transform = transform
         self.infer = infer
 
@@ -14,8 +16,8 @@ class CustomDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        img_path = self.data.iloc[idx, 1]
-        image = cv2.imread(img_path)
+        img_path = self.data.iloc[idx, 1][2:]
+        image = cv2.imread(os.path.join(self.data_root, img_path))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         
         if self.infer:
@@ -23,8 +25,8 @@ class CustomDataset(Dataset):
                 image = self.transform(image=image)['image']
             return image
         
-        mask_path = self.data.iloc[idx, 2]
-        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        mask_path = self.data.iloc[idx, 2][2:]
+        mask = cv2.imread(os.path.join(self.data_root, mask_path), cv2.IMREAD_GRAYSCALE)
         mask[mask == 255] = 12 #배경을 픽셀값 12로 간주
 
         if self.transform:
